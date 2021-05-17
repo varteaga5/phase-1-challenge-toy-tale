97c03c1d0f045481d65fd1be4c6063e8328ecd16
vincent <vincent.arteaga@gmail.com>
2021-05-17 15:28:08 -0700

    assignment complete

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ function grabToyForm() {
 // toy form event
 function submitToyForm(e) {
   e.preventDefault();
+
   const formData = {
     name : e.target[0].value,
-    img : e.target[1].value,
+    image : e.target[1].value,
     likes : 0
     
   }
@@ -26,10 +27,11 @@ function submitToyForm(e) {
     },
     body: JSON.stringify(formData)
   }
-  return fetch('http://localhost:3000/toys', configObj)
+  fetch('http://localhost:3000/toys', configObj)
   .then(resp => resp.json())
   .then(data => createCardObj(data))
   // .catch(console.log('this is catch error'))
+  e.path[0].reset();
 }
 function grabAddBtn(){
   const addBtn = document.querySelector("#new-toy-btn");
@@ -71,11 +73,12 @@ function createCard(data){
     img.classList.add('toy-avatar')
     newDiv.append(img)
     
-    // add p tag 'like' label
-    let p = document.createElement('p')
-    p.innerText = obj.likes + ' likes';
-    newDiv.append(p)
-    
+    // add p tag likes label
+    let likeLabel = document.createElement('p')
+    likeLabel.setAttribute('data-id', obj.id)
+    likeLabel.innerHTML = `${obj['likes']} ` 
+    newDiv.append(likeLabel);
+
     // add like button
     let btn = document.createElement('button')
     btn.innerText = 'like';
@@ -87,6 +90,7 @@ function createCard(data){
     document.querySelector('#toy-collection').append(newDiv)
   })
 }
+// creates card for new toys added
 function createCardObj(data){
     // new div variable
     const newDiv = document.createElement('div');
@@ -103,12 +107,14 @@ function createCardObj(data){
     img.src = data.image;
     img.classList.add('toy-avatar')
     newDiv.append(img)
-    
-    // add p tag 'like' label
-    let p = document.createElement('p')
-    p.innerText = data.likes + ' likes';
-    newDiv.append(p)
-    
+
+    // add p tag likes label
+    let likeLabel = document.createElement('p')
+    likeLabel.setAttribute('data-id', data.id)
+    likeLabel.innerHTML = `${data['likes']} likes` 
+    newDiv.append(likeLabel);
+
+
     // add like button
     let btn = document.createElement('button')
     btn.innerText = 'like';
@@ -118,22 +124,57 @@ function createCardObj(data){
     
     // append populated div to id toy-collection
     document.querySelector('#toy-collection').append(newDiv)
+    grabLikeBtn();
+
 }
 function grabLikeBtn() {
   const likeBtn = document.querySelectorAll('.like-btn')
+  console.log(likeBtn)
   likeBtn.forEach(element => element.addEventListener('click', clickedLike))
   // likeBtn.addEventListener('click', clickedLike)
 }
 function clickedLike (e) {
-  console.log('like was clicked')
-  // return fetch('http://localhost:3000/toys'+ e.target)
-  // .then(response => response.json())
-  // .then(data => createCard(data))
+
+  // console.log("checking: ", e.target.previousElementSibling)
+  // capture toys id
+  const toyId = e.target.parentElement.childNodes[3].id;
+
+  // read the current likes value from the DOM
+  // grab p tag value store in likes
+  let  likes = e.target.previousElementSibling.innerHTML;
+
+  // increase that value by 1
+  likes++
+  // // set amount equal to innerhtml of p tag
+  // e.target.previousElementSibling.innerHTML = `${likes} likes`
+
+  // pass that number along with patch request
+
+  // response received then update DOM with new number
+
+  // PATCH Request
+  const configObj = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      // can use short hand by just typing likes, short assignment
+      likes : likes
+        })
+  }
+  fetch(`http://localhost:3000/toys/${toyId}`, configObj)
+  .then(resp => resp.json())
+  .then(updateDOM)
 }
-
-
-
-
+function updateDOM (data) {
+  // grab clicked p tag by data-id
+  const pTag = document.querySelector(`[data-id= '${data.id}']`)
+  console.log('this is pTag ',pTag)
+  // update innertext to data.likes
+  pTag.innerText = data.likes;
+}
 
 
 document.addEventListener("DOMContentLoaded", init);
